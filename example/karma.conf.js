162171da0f77387c68a935f2a9a90cf236b1b833
Tim Hamilton <TimH@crossbrowsertesting.com>
2018-04-25 14:41:04 -0500

    added working example

---
+++ example/karma.conf.js
@@ -0,0 +1,46 @@
+module.exports = (config) => {
+  config.set({
+    singleRun: true,
+    frameworks: ['jasmine'],
+    plugins: [
+      'karma-*',
+      require('../index') // Path to karma-cbt-launcher
+    ],
+    preprocessors: {
+      'tests/*.html': ['html2js']
+    },
+    files: [
+      'tests/*.js',
+      'tests/index.html'
+    ],
+    logLevel: config.LOG_DEBUG,
+    browsers: [
+      'win7_ie11',
+      'win10_edge20',
+    ],
+    reporters: [
+      'progress',
+      'CrossBrowserTesting',
+    ],
+    cbtConfig: {
+      username: '',
+      authkey: '',
+    },
+    customLaunchers: {
+      win7_ie11: {
+        base: 'CrossBrowserTesting',
+        browserName: 'win7_ie11',
+        browser_api_name: 'IE11',
+        os_api_name: 'Win7x64',
+        screen_resolution: '1366x768',
+      },
+      win10_edge20: {
+        base: 'CrossBrowserTesting',
+        browserName: 'win10_edge20',
+        browser_api_name: 'Edge20',
+        os_api_name: 'Win10',
+        screen_resolution: '1920x1080',
+      },
+    },
+  })
+}
